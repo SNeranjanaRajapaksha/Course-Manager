1fd805a16beb105c12ebb7d83f734b2822927773
SNeranjanaRajapaksha <snrajapaksha@std.appsc.sab.ac.lk>
2025-01-16 23:45:03 +0530

    update

--- database/migrations/2025_01_13_173427_create_lessons_table.php
+++ database/migrations/2025_01_13_173427_create_lessons_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations for the table `lessons`.
      */
     public function up(): void
     {
